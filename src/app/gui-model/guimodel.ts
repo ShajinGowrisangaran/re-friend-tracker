e1cb46532344905d345170eadbf66e43a588c47b
ShajinGowrisangaran <65170316+ShajinGowrisangaran@users.noreply.github.com>
2020-05-19 23:24:56 +0200

    Add files via upload

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Shajins Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -28,6 +28,21 @@ export class GuiModel {
                             "required": true
                         },
                         {
+                            "id":"nickname",
+                            "type":"text",
+                            "name":"Nickname",
+                            "width":2,
+                            "required":true
+                        },
+                        {
+                            "id": "group",
+                            "type":"autocomplete",
+                            "name":"Group",
+                            "url":"/group",
+                            "form":"GroupForm",
+                            "width":2
+                        },
+                        {        
                             "id":   "location",
                             "type": "autocomplete",
                             "name": "Location",
@@ -118,6 +133,32 @@ export class GuiModel {
                         }
                     ]
                 },
+                {
+                    "id":"GroupForm",
+                    "title":"Group",
+                    "url":"/group",
+                    "formFieldList":[
+                        {
+                            "id":"name",
+                            "type":"text",
+                            "name":"GroupName",
+                            "width":2,
+                            "required":true
+                        },
+                        {
+                            "type":"deleteButton",
+                            "name":"Delete"
+                        },
+                        {
+                            "type":"cancelButton",
+                            "name":"Cancel"
+                        },
+                        {
+                            "type":"okButton",
+                            "name":"Ok"
+                        }
+                    ]
+                }
             ],
             "pageList": [
                 {
@@ -133,11 +174,24 @@ export class GuiModel {
                         },
                         {
                             "type": "button",
+                            "name":"Activity",
+                            "color":"red",
+                            "page":"activitiespage",
+                        },
+                        {    
+                            "type": "button",
                             "name": "Location",
                             "icon": "fa-cubes",
                             "color": "yellow",
                             "page": "locationspage",
                         },
+                        {
+                            "type":"button",
+                            "name":"Groups",
+                            "icon":"fa-weixin",
+                            "color":"wisteria",
+                            "page":"groupspage",
+                        }
                     ]
                 },
                 {
@@ -195,6 +249,33 @@ export class GuiModel {
                         },
                     ]
                 },
+                {
+                    "id":"groupspage",
+                    "elementList":[
+                        {
+                            "type":"backbutton",
+                        },
+                        {
+                            "type":"newButton",
+                            "name":"NewGroup",
+                            "icon":"fa-weixin",
+                            "color":"green",
+                            "form":{
+                                "form":"GroupForm"
+                            }
+                        },
+                        {
+                            "type":"list",
+                            "icon":"fa-weixin",
+                            "color":"wisteria",
+                            "search":true,
+                            "url":"/group",
+                            "form":{
+                                "form":"GroupForm"
+                            }
+                        }
+                    ]
+                }
             ]
         }
     };
